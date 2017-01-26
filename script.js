7e9d7efe1c9b5ac7a5009cc70e2ee6b2375e6912
juliakent77 <juliakent@Julias-MacBook-Air.local>
2017-01-26 15:28:54 -0800

    javascript if else statement and image updates

--- script.js
+++ script.js
@@ -1,107 +1,53 @@
-// How to get the div clicks to input tags within my array?
-// How to add event listeners on my divs
-
-
-// An array of my recipes:
-// [
+// var drinkRecipes [
 // {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} 
-// {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
+// {name:'Pear Martini', image:'src="Images/pearMartini.jpg"', drinkType: 'vodka', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} 
+// {name:'Blueberry Mojitos', image:'src="Images/mojito.jpg"', drinkType: 'rum', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} 
+// {name:'Paloma', image:'src="Images/paloma.jpg"', drinkType: 'tequila', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} 
+// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} 
+// {name:'Gin Fizz', image:'src="Images/ginDrink.jpg"', drinkType: 'gin', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}]
 
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
+$(document).ready(function() {
+  if(window.location.pathname === '/recipe.html' && localStorage.getItem('drinkType') && localStorage.getItem('flavor')) {
+    var drinkType = localStorage.getItem('drinkType');
+    var flavor = localStorage.getItem('flavor');
+  }
 
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
+  // for (i = 0; i < fLen; i++) {
+  //   text += "<li>" + fruits[i] + "</li>";
+  // }
 
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'botnaical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'smoky', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'spicy', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-// {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'frosty', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}
-
-// ]
-
-// [{name:'Old Fashioned', image:'src="Images/drank.jpg"', drinkType: 'whiskey', flavor: 'sour', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} {name:'Pear Martini', image:'src="Images/drank.jpg"', drinkType: 'vodka', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} {name:'Dark and Stormy', image:'src="Images/drank.jpg"', drinkType: 'rum', flavor: 'botanical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} {name:'Paloma', image:'src="Images/drank.jpg"', drinkType: 'tequila', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} {name:'Peach Bellini', image:'src="Images/drank.jpg"', drinkType: 'champagne', flavor: 'fruity', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'} {name:'Gin Fizz', image:'src="Images/drank.jpg"', drinkType: 'gin', flavor: 'botnaical', strength:'xxx', ingredients: 'list items goes here', recipe: 'Recipe goes heres', music: 'url=""'}]
+var checkRecipe = function() {
+  if(drinkType === 'whiskey') {
+    if(flavor === 'sour') {
+      console.log('drink one');
+    } else if (flavor === 'fruity') {
+      console.log('drink two');
+    } else if (flavor === 'botanical') {
+      console.log('drink three'); 
+    } else if(flavor === 'smoky') {
+      console.log('drink four');
+    } else if (flavor === 'spicy') {
+      console.log('drink five');
+    } else if (flavor == 'frosty') {
+      console.log('drink six');
+    }
+  } 
+}
 
 
-
-$(".alc").click(function(event) {
+  $(".alc").click(function(event) {
     var elem = $(this);
     var drinkType = elem.attr('drink');
     
     localStorage.setItem('drinkType', drinkType);
     window.location = 'flavor.html'
-});
+  });
 
-$(".taste").click(function(event) {
+  $(".taste").click(function(event) {
     var elem = $(this);
     var flavorType = elem.attr('flavor');
 
     localStorage.setItem('flavorType', flavorType);
     window.location = 'recipe.html'
+  });
 });
-
-$( "#result" ).load( "recipe.html", function() {
-  alert( "Load was performed." );
-});
-
-// $('.recipeContainer').load(function(event) {
-//   alert( "Load was performed." );
-//   // event.preventDefault();
-//   // var elem = $(event.target);
-
-//   // // var todo = elem.todoInput.val();
-//   // var todo = $("input:text").val();
-
-//   // var li = $(add.('li'));
-//   // var input = $(add.('input'));
-//   // var p = $(add.('p'));
-//   // var deleteButton = $(add.('button'));
-//   // var clearDiv = $(add.('div'));
-
-//   // li.className = $('fadeIn');
-//   // input.type = $('checkbox');
-//   // p.innerHTML = $(todo);
-//   // deleteButton.innerHTML = $('Delete');
-//   // clearDiv.className = $('clearfix');
-
-//   // $(deleteButton).click(function(event) {
-//   //   $(elem).parent().remove();
-//   // };
-
-//   // $('li').append(input);
-//   // $('li').append(p);
-//   // $('li').append(deleteButton);
-//   // $('li').append(clearDiv);
-
-//   // $('todoList').append(li);
-
-//   // elem.todoInput.value = '';
-// });
